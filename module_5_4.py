e3749d5df2284632cad65d8402c357ee9941430f
klaviwka <klaviwka@mail.ru>
2024-08-22 21:20:43 +0300

    Add files via upload

--- module_5_4.py
+++ module_5_4.py
@@ -4,21 +4,26 @@ class House:
     def __init__(self, name, height):
         self.name = name
         self.height = height
-        House.houses_history.append(self.name)
 
     def __del__(self):
         print(f"{self.name} снесён, но он останется в истории")
         House.houses_history.remove(self.name)
 
     def new(self, name, height):
-        return House(name, height)
+        new_house = House(name, height)
+        House.houses_history.append(new_house.name)
+        return new_house
 
-h1 = House('ЖК Эльбрус', 10)
+
+house_creator = House('Создатель', 0)
+
+h1 = house_creator.new('ЖК Эльбрус', 10)
 print(House.houses_history)
-h2 = House('ЖК Акация', 20)
+h2 = house_creator.new('ЖК Акация', 20)
 print(House.houses_history)
-h3 = House('ЖК Матрёшки', 20)
+h3 = house_creator.new('ЖК Матрёшки', 20)
 print(House.houses_history)
+
 del h2
 del h3
 
